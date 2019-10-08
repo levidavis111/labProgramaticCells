1c151ff558ad2b748e6cdd8c152ee22109cb6e73
levi davis <levidavis@pursuit.org>
2019-10-08 14:43:53 -0400

    First commit

---
+++ ProgrammaticUserCells/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  ProgrammaticUserCells
+//
+//  Created by Levi Davis on 10/8/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ProgrammaticUserCells/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        
+        
+        
+        return tableView
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        
+    }
+
+    private func addSubViews() {
+        
+    }
+
+}
+
