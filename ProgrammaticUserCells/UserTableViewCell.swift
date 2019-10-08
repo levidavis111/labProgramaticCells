d186eadfd640cded41640ef8ea36339143615684
levi davis <levidavis@pursuit.org>
2019-10-08 15:03:41 -0400

    Adds label to table view cell

--- ProgrammaticUserCells/UserTableViewCell.swift
+++ ProgrammaticUserCells/UserTableViewCell.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class UserTableViewCell: UITableViewCell {
     
+    lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.textColor = .black
+        nameLabel.textAlignment = .center
+        nameLabel.lineBreakMode = .byWordWrapping
+        nameLabel.numberOfLines = 0
+        
+        return nameLabel
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(nameLabel)
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -27,6 +39,14 @@ class UserTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func setConstraints() {
+        self.nameLabel.translatesAutoresizingMaskIntoConstraints = true
+        self.nameLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+        self.nameLabel.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+        self.nameLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        self.nameLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 
 }
