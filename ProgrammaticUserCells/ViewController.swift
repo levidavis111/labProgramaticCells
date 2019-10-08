bf07951ce7a54dc74f88cc77d36f899e1b5f6f9c
levi davis <levidavis@pursuit.org>
2019-10-08 14:52:32 -0400

    Adds table view programatically

--- ProgrammaticUserCells/ViewController.swift
+++ ProgrammaticUserCells/ViewController.swift
@@ -13,13 +13,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubViews()
-        
+        addSubviews()
+        setConstraints()
     }
 
-    private func addSubViews() {
-        
+    private func addSubviews() {
+        self.view.addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+
+    }
+    
+    private func setDelegates() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {}
